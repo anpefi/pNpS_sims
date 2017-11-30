804b4324f2d9049ec5466a11472aa975149fa26b
Andrés Pérez-Figueroa <andres.perez.figueroa@gmail.com>
2017-11-30 20:22:30 +0100

    Fixed bad parameter values and more additions
    finalTime and keepEvery parameters in the sampling function of the run_case_neutral.R script were set in fixed values instead of passing the parameters declared at the beginning ofthe script. A column for the number of drivers mutations detected is included in the sampling output. Output as a tibble.

--- scripts/run_case_neutral.R
+++ scripts/run_case_neutral.R
@@ -5,7 +5,7 @@ library(tidyverse)
 ## Set the parameters
 
 # Number of loci. N/S ~ 2.76 following Mulholland et al 2017
-nNS_pos <- 10 
+nNS_pos <- 10
 nNS_neg <- 10
 nNS_neu <- 27580
 nNS <- nNS_pos + nNS_neg + nNS_neu
@@ -22,7 +22,7 @@ mu <- 2.66e-9  #Following Milholland et al 2017
 loci <- c(sNS_pos,sNS_neg,sNS_neu, sS)
 names(loci) <- 1:length(loci)
 # Setting the Fitnes effect on the simulator
-fe <- allFitnessEffects(noIntGenes = loci, drvNames = names(loci[1:(nNS_pos+1)]) )
+fe <- allFitnessEffects(noIntGenes = loci, drvNames = names(loci[1:nNS_pos]) )
 
 #Simulation settings
 simName <- "neutral"
@@ -33,15 +33,16 @@ detectionSize <- 1e9 #Population size threshold to stop the simulation
 sampleEvery <- 10 #Time unit to check status of the population
 keepEvery <- 100 #Time units to keep samples for further analysis
 mutationPropGroth <- FALSE  #Is the mutation rate proportional to the pop growth rate?
-mc.cores <- 5 #cores to use
+mc.cores <- 1 #cores to use
+det.limit <- 0.05  #Threshold for a variant frequency to be detected
 
 ### SIMULATION
 #Reproducibility setting
 RNGkind("Mersenne-Twister")
-set.seed(2134)
+set.seed(1234)
 
-#Run 
-pp <- oncoSimulPop(nreps, fe, mu= mu, onlyCancer = F, detectionSize = detectionSize, 
+#Run
+pp <- oncoSimulPop(nreps, fe, mu= mu, onlyCancer = F, detectionSize = detectionSize,
                    detectionDrivers = NA, detectionProb = NA, sampleEvery=sampleEvery,
                    initSize = initSize, finalTime = finalTime, keepEvery=keepEvery,
                    mutationPropGrowth = FALSE, mc.cores = mc.cores)
@@ -57,33 +58,35 @@ NSsample <- function(x, det.limit=0.05, nNS, nS, finalTime, keepEvery){
   }
   tsamples <- seq(0, finalTime, keepEvery)
   n <- length(tsamples)
-  result <- data.frame(t=tsamples,
+  result <- tibble(t=tsamples,
                        TS=rep(0,n),
-                       N=rep(NA,n),S=rep(NA,n),pN_pS=rep(NA,n))
+                       N=rep(NA,n),S=rep(NA,n), nDrivers=rep(0,n),
+                       pN_pS=rep(NA,n))
    for (i in 1:n){
     if (!is.element(x$pops.by.time[i,1], tsamples)) break
-  
+
     pop <- x$pops.by.time[i, -1]
-    
+
     if(all(pop == 0)) {
       #result <- rbind(result, c(i,0,NA,NA,NA))
       break
     }
-    
+
     popSize <- x$PerSampleStats[i, 1]
     variants <- as.numeric((tcrossprod(pop, x$Genotypes)/popSize) >= det.limit)
+    ndrivers <- sum(variants[1:nNS_pos])
     N <-sum(variants[1:nNS])
     S <-sum(variants[nNS+1:nS])
     if (S>0) pNpS <- (N / nNS) / (S / nS)
     else pNpS <- NA
-    result[i,] <- c(tsamples[i],popSize,N,S,pNpS)
+    result[i,] <- c(tsamples[i],popSize,N,S,ndrivers,pNpS)
   }
-  
+
   return(result)
-  
+
 }
 
-system.time(samples <- do.call(rbind, Map(NSsample, pp, det.limit=0.05, nNS=nNS, nS=nS, finalTime=10000, keepEvery=100)) )
+samples <- do.call(rbind, Map(NSsample, pp, det.limit=det.limit, nNS=nNS, nS=nS, finalTime=finalTime, keepEvery=keepEvery))
 
 saveRDS(as.tibble(samples), file=paste0(simName,".samples.gz"), compress = "gzip")
 
