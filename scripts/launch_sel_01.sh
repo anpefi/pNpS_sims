3ae8d13c5a077e5ee165be3d296bc4af22457486
Andrés Pérez-Figueroa <andres.perez.figueroa@gmail.com>
2018-01-17 16:01:46 +0100

    Add launcher scripts for selective tests
    Added two lwauncher scripts for two selective cases Sel01 and Se02 with
    500 pos + 500 neg non synonymous mutations and 500 + 5000 respectively.
    These cases were run as examples for a small lab meeting.

---
+++ scripts/launch_sel_01.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH -J sel_01_McFL           # Job name
+#SBATCH -o sel_01_McFL-%a.out    # Specify stdout output file (%j expands to jobId)
+#SBATCH -a 1-10
+#SBATCH -n 1                     # Total number of tasks
+#SBATCH -t 08:00:00
+#SBATCH --mem-per-cpu 20GB
+
+set -e
+
+## launch_simulations.sh
+
+#load modules
+module load gcc R
+
+# DEFINE ENVIRONMENT VARIABLES
+export PROJ_DIR=$HOME/pNpS.sims
+export SCRIPT_DIR=$PROJ_DIR/scripts
+export SCRATCH=$PROJ_DIR/scratch
+export RES_DIR=$PROJ_DIR/results
+
+cd $RES_DIR
+NAME=sel_01_McFL_$SLURM_ARRAY_TASK_ID
+$SCRIPT_DIR/run_simulation.R -i $NAME --seed 0 --nNS_pos 500 --nNS_neg 500 \
+          --nNS_neu 26600 -c 1 --sampleEvery 0.0025 --reps 1 --model McFL \
+          --finalTime 1000 --initSize 100000 --keepEvery 5 \
+          --detectionSize 1e08
+$SCRIPT_DIR/run_sampling.R -i $NAME --dir results
+
+
