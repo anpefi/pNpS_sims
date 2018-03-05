cf6c0c95b4c51c8d8c2c89386502181029a99f66
Andrés Pérez-Figueroa <andres.perez.figueroa@gmail.com>
2018-03-05 10:28:45 +0100

    Change threshold in the old simulations
    This was an unstaged change from the las month. As I am cleaning up all
    the repo for the forthcoming changes I prefer to make this change
    explicit in the history.

--- scripts/run_sampling.R
+++ scripts/run_sampling.R
@@ -15,7 +15,7 @@ opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 opt_sim <- readRDS(file = here::here(opt$dir,paste0(opt$id,".opt.rds")))
 # In case they are argument shared, this call will update the simulation ones
 opt <- c( opt_sim[!is.element(names(opt_sim), names(opt))] , opt )
-opt$det.limit <- c(1e-09, 0.05)
+opt$det.limit <- c(0, 0.05)
 
 ### LOADING SIMULATION DATA
 pp <- readRDS(file = here::here(opt$dir,paste0(opt$id,".pop.rds")))
