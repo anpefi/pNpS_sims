eed6525e85f21332d9bffae8b814954f5c700309
Andrés Pérez-Figueroa <andres.perez.figueroa@gmail.com>
2021-11-26 00:36:48 +0100

    Add consolidation scripts. Remove unused slim scripts

---
+++ scripts/consolidate_results.sh
@@ -0,0 +1,39 @@
+cat results/pos_0.1_neg_0_*.freqs.csv > temp && cat temp >> results/pos_0.1_neg_0.freqs.csv
+cat results/pos_0.1_neg_0.1_*.freqs.csv > temp && cat temp >> results/pos_0.1_neg_0.1.freqs.csv
+cat results/pos_0.1_neg_0.25_*.freqs.csv > temp && cat temp >> results/pos_0.1_neg_0.25.freqs.csv
+cat results/pos_0.1_neg_0.5_*.freqs.csv > temp && cat temp >> results/pos_0.1_neg_0.5.freqs.csv
+cat results/pos_0.1_neg_0.75_*.freqs.csv > temp && cat temp >> results/pos_0.1_neg_0.75.freqs.csv
+cat results/pos_0.1_neg_1_*.freqs.csv > temp && cat temp >> results/pos_0.1_neg_1.freqs.csv
+rm results/pos_0.1_neg_*_*.freqs.csv
+
+cat results/pos_0.25_neg_0_*.freqs.csv > temp && cat temp >> results/pos_0.25_neg_0.freqs.csv
+cat results/pos_0.25_neg_0.1_*.freqs.csv > temp && cat temp >> results/pos_0.25_neg_0.1.freqs.csv
+cat results/pos_0.25_neg_0.25_*.freqs.csv > temp && cat temp >> results/pos_0.25_neg_0.25.freqs.csv
+cat results/pos_0.25_neg_0.5_*.freqs.csv > temp && cat temp >> results/pos_0.25_neg_0.5.freqs.csv
+cat results/pos_0.25_neg_0.75_*.freqs.csv > temp && cat temp >> results/pos_0.25_neg_0.75.freqs.csv
+cat results/pos_0.25_neg_1_*.freqs.csv > temp && cat temp >> results/pos_0.25_neg_1.freqs.csv
+rm results/pos_0.25_neg_*_*.freqs.csv
+
+cat results/pos_0.5_neg_0_*.freqs.csv > temp && cat temp >> results/pos_0.5_neg_0.freqs.csv
+cat results/pos_0.5_neg_0.1_*.freqs.csv > temp && cat temp >> results/pos_0.5_neg_0.1.freqs.csv
+cat results/pos_0.5_neg_0.25_*.freqs.csv > temp && cat temp >> results/pos_0.5_neg_0.25.freqs.csv
+cat results/pos_0.5_neg_0.5_*.freqs.csv > temp && cat temp >> results/pos_0.5_neg_0.5.freqs.csv
+cat results/pos_0.5_neg_0.75_*.freqs.csv > temp && cat temp >> results/pos_0.5_neg_0.75.freqs.csv
+cat results/pos_0.5_neg_1_*.freqs.csv > temp && cat temp >> results/pos_0.5_neg_1.freqs.csv
+rm results/pos_0.5_neg_*_*.freqs.csv
+
+cat results/pos_0.75_neg_0_*.freqs.csv > temp && cat temp >> results/pos_0.75_neg_0.freqs.csv
+cat results/pos_0.75_neg_0.1_*.freqs.csv > temp && cat temp >> results/pos_0.75_neg_0.1.freqs.csv
+cat results/pos_0.75_neg_0.25_*.freqs.csv > temp && cat temp >> results/pos_0.75_neg_0.25.freqs.csv
+cat results/pos_0.75_neg_0.5_*.freqs.csv > temp && cat temp >> results/pos_0.75_neg_0.5.freqs.csv
+cat results/pos_0.75_neg_0.75_*.freqs.csv > temp && cat temp >> results/pos_0.75_neg_0.75.freqs.csv
+cat results/pos_0.75_neg_1_*.freqs.csv > temp && cat temp >> results/pos_0.75_neg_1.freqs.csv
+rm results/pos_0.75_neg_*_*.freqs.csv
+
+cat results/pos_1_neg_0_*.freqs.csv > temp && cat temp >> results/pos_1_neg_0.freqs.csv
+cat results/pos_1_neg_0.1_*.freqs.csv > temp && cat temp >> results/pos_1_neg_0.1.freqs.csv
+cat results/pos_1_neg_0.25_*.freqs.csv > temp && cat temp >> results/pos_1_neg_0.25.freqs.csv
+cat results/pos_1_neg_0.5_*.freqs.csv > temp && cat temp >> results/pos_1_neg_0.5.freqs.csv
+cat results/pos_1_neg_0.75_*.freqs.csv > temp && cat temp >> results/pos_1_neg_0.75.freqs.csv
+cat results/pos_1_neg_1_*.freqs.csv > temp && cat temp >> results/pos_1_neg_1.freqs.csv
+rm results/pos_1_neg_*_*.freqs.csv
---
+++ scripts/consolidate_results3.sh
@@ -0,0 +1,39 @@
+cat results/pos_0.1_neg_0_*.result.csv > temp && cat temp >> results/pos_0.1_neg_0.result.csv
+cat results/pos_0.1_neg_0.1_*.result.csv > temp && cat temp >> results/pos_0.1_neg_0.1.result.csv
+cat results/pos_0.1_neg_0.25_*.result.csv > temp && cat temp >> results/pos_0.1_neg_0.25.result.csv
+cat results/pos_0.1_neg_0.5_*.result.csv > temp && cat temp >> results/pos_0.1_neg_0.5.result.csv
+cat results/pos_0.1_neg_0.75_*.result.csv > temp && cat temp >> results/pos_0.1_neg_0.75.result.csv
+cat results/pos_0.1_neg_1_*.result.csv > temp && cat temp >> results/pos_0.1_neg_1.result.csv
+rm results/pos_0.1_neg_*_*.result.csv
+
+cat results/pos_0.25_neg_0_*.result.csv > temp && cat temp >> results/pos_0.25_neg_0.result.csv
+cat results/pos_0.25_neg_0.1_*.result.csv > temp && cat temp >> results/pos_0.25_neg_0.1.result.csv
+cat results/pos_0.25_neg_0.25_*.result.csv > temp && cat temp >> results/pos_0.25_neg_0.25.result.csv
+cat results/pos_0.25_neg_0.5_*.result.csv > temp && cat temp >> results/pos_0.25_neg_0.5.result.csv
+cat results/pos_0.25_neg_0.75_*.result.csv > temp && cat temp >> results/pos_0.25_neg_0.75.result.csv
+cat results/pos_0.25_neg_1_*.result.csv > temp && cat temp >> results/pos_0.25_neg_1.result.csv
+rm results/pos_0.25_neg_*_*.result.csv
+
+cat results/pos_0.5_neg_0_*.result.csv > temp && cat temp >> results/pos_0.5_neg_0.result.csv
+cat results/pos_0.5_neg_0.1_*.result.csv > temp && cat temp >> results/pos_0.5_neg_0.1.result.csv
+cat results/pos_0.5_neg_0.25_*.result.csv > temp && cat temp >> results/pos_0.5_neg_0.25.result.csv
+cat results/pos_0.5_neg_0.5_*.result.csv > temp && cat temp >> results/pos_0.5_neg_0.5.result.csv
+cat results/pos_0.5_neg_0.75_*.result.csv > temp && cat temp >> results/pos_0.5_neg_0.75.result.csv
+cat results/pos_0.5_neg_1_*.result.csv > temp && cat temp >> results/pos_0.5_neg_1.result.csv
+rm results/pos_0.5_neg_*_*.result.csv
+
+cat results/pos_0.75_neg_0_*.result.csv > temp && cat temp >> results/pos_0.75_neg_0.result.csv
+cat results/pos_0.75_neg_0.1_*.result.csv > temp && cat temp >> results/pos_0.75_neg_0.1.result.csv
+cat results/pos_0.75_neg_0.25_*.result.csv > temp && cat temp >> results/pos_0.75_neg_0.25.result.csv
+cat results/pos_0.75_neg_0.5_*.result.csv > temp && cat temp >> results/pos_0.75_neg_0.5.result.csv
+cat results/pos_0.75_neg_0.75_*.result.csv > temp && cat temp >> results/pos_0.75_neg_0.75.result.csv
+cat results/pos_0.75_neg_1_*.result.csv > temp && cat temp >> results/pos_0.75_neg_1.result.csv
+rm results/pos_0.75_neg_*_*.result.csv
+
+cat results/pos_1_neg_0_*.result.csv > temp && cat temp >> results/pos_1_neg_0.result.csv
+cat results/pos_1_neg_0.1_*.result.csv > temp && cat temp >> results/pos_1_neg_0.1.result.csv
+cat results/pos_1_neg_0.25_*.result.csv > temp && cat temp >> results/pos_1_neg_0.25.result.csv
+cat results/pos_1_neg_0.5_*.result.csv > temp && cat temp >> results/pos_1_neg_0.5.result.csv
+cat results/pos_1_neg_0.75_*.result.csv > temp && cat temp >> results/pos_1_neg_0.75.result.csv
+cat results/pos_1_neg_1_*.result.csv > temp && cat temp >> results/pos_1_neg_1.result.csv
+rm results/pos_1_neg_*_*.result.csv
